a489ac4d6da4b9590a5c755f64ca9b1acba5a9ac
LeAnhHuy2409 <huyleanh2409@gmail.com>
2024-11-08 15:14:58 +0700

    Delete Buoi_6 directory

--- Buoi_6/src/BT/Main.java
+++
@@ -1,9 +0,0 @@
-package BT;
-
-public class Main {
-public static void main(String[] args) {
-	Mobile mobile = new Mobile("M001", "Smartphone",300.0,10."Samsung",8);
-	System.out.println(mobile);
-	
-}
-}
